b06bb34f0db418f9c13ba88ce21b99704c8d76cf
Greg Wilson <greg.wilson@plot.ly>
2025-04-02 14:55:28 -0400

    feat: generating random values for grid cells based on distance from center

--- src/snailz/grids.py
+++ src/snailz/grids.py
@@ -1,6 +1,8 @@
 """Generate random grids."""
 
+from collections import defaultdict
 import io
+import math
 import random
 
 from pydantic import BaseModel, Field
@@ -11,6 +13,7 @@ from . import utils
 class GridParams(BaseModel):
     """Parameters for grid generation."""
 
+    limit: float = Field(default=10.0, gt=0.0, description="Maximum pollution level")
     number: int = Field(default=3, gt=0, description="Number of grids")
     size: int = Field(default=15, gt=0, description="Grid size")
 
@@ -53,7 +56,7 @@ def grids_generate(parameters):
     gen = utils.UniqueIdGenerator("grid", _grid_id_generator)
     grids = []
     for _ in range(parameters.number):
-        grids.append(_make_grid(gen.next(), parameters.size))
+        grids.append(_make_grid(parameters, gen.next()))
     return GridList(grids=grids)
 
 
@@ -64,8 +67,40 @@ def _grid_id_generator():
     return f"G{num:03d}"
 
 
-def _make_grid(ident, size):
-    """Create a grid of specified size."""
+def _make_grid(params, ident):
+    """Create a grid of specified size and fill with random values."""
 
-    cells = [[0 for _ in range(size)] for _ in range(size)]
-    return Grid(ident=ident, size=size, cells=cells)
+    cells = [[0.0 for _ in range(params.size)] for _ in range(params.size)]
+    center = params.size // 2
+    cells[center][center] = random.uniform(params.limit / 2, params.limit)
+
+    radial_groups = _make_radial_groups(params.size)
+    for inv_dist in sorted(radial_groups.keys()):
+        points = radial_groups[inv_dist]
+        temp = []
+        for (x, y) in points:
+            temp.append((x, y, _make_value(cells, x, y, params.limit, inv_dist)))
+        for (x, y, val) in temp:
+            cells[x][y] = val
+
+    return Grid(ident=ident, size=params.size, cells=cells)
+
+
+def _make_radial_groups(size):
+    """Group points by distance from center."""
+    groups = defaultdict(set)
+    center = size // 2
+    for x in range(size):
+        for y in range(size):
+            if (x == center) and (y == center):
+                continue
+            inv_dist = center - math.sqrt((x - center) ** 2 + (y - center) ** 2) / size
+            groups[inv_dist].add((x, y))
+    return groups
+
+
+def _make_value(cells, x, y, limit, inv_dist):
+    """Generate a single cell value."""
+
+    cutoff = limit * inv_dist
+    return random.uniform(cutoff / 2, cutoff)
