da82d52fb92d2e1f84b2343aad8ee98e8133d106
Greg Wilson <gvwilson@third-bit.com>
2026-01-21 20:05:00 -0500

    adding ratings

--- src/snailz/main.py
+++ src/snailz/main.py
@@ -8,11 +8,12 @@ import random
 import sqlite3
 import sys
 
-from .effects import do_all_effects
+from .effect import do_all_effects
 from .grid import Grid
-from .machines import Machine
+from .machine import Machine
 from .parameters import Parameters
 from .person import Person
+from .rating import Rating
 from .sample import Sample
 from . import utils
 
@@ -38,6 +39,17 @@ create table person (
 """
 INSERT_PERSON = """\
 insert into person values (?, ?, ?);
+"""
+
+CREATE_RATING = """\
+create table rating (
+    person_id text not null,
+    machine_id text not null,
+    rating integer
+);
+"""
+INSERT_RATING = """\
+insert into rating values(?, ?, ?);
 """
 
 CREATE_SAMPLE = """\
@@ -67,10 +79,10 @@ def main():
         return 0
 
     params = _initialize(args)
-    grids, persons, samples, machines = _synthesize(params)
+    grids, persons, samples, machines, ratings = _synthesize(params)
     changes = do_all_effects(params, grids, persons, samples)
-    if args.outdir:
-        _save(args, grids, persons, samples, machines, changes)
+    _save(args, grids, persons, samples, machines, ratings, changes)
+    _save_db(args)
 
     return 0
 
@@ -101,11 +113,15 @@ def _parse_args():
     return parser.parse_args()
 
 
-def _save(args, grids, persons, samples, machines, changes):
+def _save(args, grids, persons, samples, machines, ratings, changes):
     """Save synthesized data."""
 
-    if args.outdir == "-":
+    if not args.outdir:
+        return
+
+    elif args.outdir == "-":
         outdir = None
+
     else:
         outdir = Path(args.outdir)
         if not outdir.is_dir():
@@ -121,6 +137,7 @@ def _save(args, grids, persons, samples, machines, changes):
     for name, cls, data in (
         ("machines", Machine, machines),
         ("persons", Person, persons),
+        ("ratings", Rating, ratings),
         ("samples", Sample, samples),
     ):
         with utils.file_or_std(outdir, f"{name}.csv", "w") as writer:
@@ -129,22 +146,29 @@ def _save(args, grids, persons, samples, machines, changes):
     with utils.file_or_std(outdir, "changes.json", "w") as writer:
         json.dump(changes, writer)
 
-    if args.outdir != "-":
-        db_path = Path(outdir, "snailz.db")
-        db_path.unlink(missing_ok=True)
-        cnx = sqlite3.connect(db_path)
-        cur = cnx.cursor()
-        for name, create, insert in (
-            ("grids", CREATE_GRID, INSERT_GRID),
-            ("persons", CREATE_PERSON, INSERT_PERSON),
-            ("samples", CREATE_SAMPLE, INSERT_SAMPLE),
-        ):
-            with open(Path(args.outdir, f"{name}.csv"), "r") as reader:
-                rows = [r for r in csv.reader(reader)]
-                cur.execute(create)
-                cur.executemany(insert, rows[1:])
-        cnx.commit()
-        cnx.close()
+
+def _save_db(args):
+    """Convert saved data to SQLite database."""
+
+    if (args.outdir is None) or (args.outdir == "-"):
+        return
+
+    db_path = Path(args.outdir, "snailz.db")
+    db_path.unlink(missing_ok=True)
+    cnx = sqlite3.connect(db_path)
+    cur = cnx.cursor()
+    for name, create, insert in (
+        ("grids", CREATE_GRID, INSERT_GRID),
+        ("persons", CREATE_PERSON, INSERT_PERSON),
+        ("ratings", CREATE_RATING, INSERT_RATING),
+        ("samples", CREATE_SAMPLE, INSERT_SAMPLE),
+    ):
+        with open(Path(args.outdir, f"{name}.csv"), "r") as reader:
+            rows = [r for r in csv.reader(reader)]
+            cur.execute(create)
+            cur.executemany(insert, rows[1:])
+    cnx.commit()
+    cnx.close()
 
 
 def _synthesize(params):
@@ -154,7 +178,8 @@ def _synthesize(params):
     persons = Person.make(params)
     samples = Sample.make(params, grids, persons)
     machines = Machine.make(params)
-    return grids, persons, samples, machines
+    ratings = Rating.make(persons, machines)
+    return grids, persons, samples, machines, ratings
 
 
 if __name__ == "__main__":
